b8382539e0cb06e56b1371ba77fc106f20ff82c8
JavierLopezDev <javierlopezandujar2003@gmail.com>
2024-02-01 19:44:16 +0100

    Initial commit

---
+++ app/src/main/java/com/dam/maestrodelamultiplicacion/ui/Entrenar/EntrenarFragment.java
@@ -0,0 +1,37 @@
+package com.dam.maestrodelamultiplicacion.ui.Entrenar;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.dam.maestrodelamultiplicacion.databinding.FragmentHomeBinding;
+
+public class EntrenarFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        HomeViewModel homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textHome;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
